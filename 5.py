bda68b7826d9318f9b1a676c3f3baedd739580db
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2022-12-05 14:43:50 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,17 +1,35 @@
-def ok(a,b):
-	if a[1] < b[0] or b[1] < a[0]:
-		return 0
-	return 1
+tot = 9
+
+a = [[] for x in range(tot)]
 
 with open("in.txt") as fp:
 	Lines = fp.readlines()
-	tot = 0
-	ans = 0
+	flag = True
 	for line in Lines:
-		x = line.strip()
-		x = x.split(',')
-		for i in range(len(x)):
-			x[i] = [int(y) for y in x[i].split('-')]
-		ans += ok(x[0],x[1])
-
-print(ans)
+		x = line
+		if len(x) <= 1:
+			continue
+		
+		if x[1] == '1':
+			flag = False
+			continue
+		
+		if flag:
+			j = 0
+			for i in range(1,len(x),4):
+				if x[i] != ' ':
+					a[j].append(x[i])
+				j += 1
+		
+		else:
+			x = x.strip()
+			cnt = int(x[x.find("move ") + 5:x.find(" from")])
+			s = int(x[x.find("from") + 5:x.find(" to")]) - 1
+			d = int(x[x.find("to ") + 3:]) - 1
+			
+			# a[d] = a[s][cnt-1::-1] + a[d]
+			a[d] = a[s][:cnt] + a[d]
+			a[s] = a[s][cnt:]
+		
+for i in range(tot):
+	print(a[i][0],end="")
